acc2b70eb29f2d855e97fdcf4933bee1185d35ed
piyush bharade <piyushbharade2@gmail.com>
2024-07-04 18:43:21 +0530

    first commit

---
+++ frontend/src/config/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCasYex9fvSX51zNOUUQsAO4veJ9GGFS5A",
+  authDomain: "yoga-master-final-d559c.firebaseapp.com",
+  projectId: "yoga-master-final-d559c",
+  storageBucket: "yoga-master-final-d559c.appspot.com",
+  messagingSenderId: "374287436664",
+  appId: "1:374287436664:web:27c56003d21db36386d68d"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ frontend/src/pages/Home/Map/Map.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CountUp from 'react-countup';
+
+const Map = () => {
+   
+}; 
+
+export default Map;
